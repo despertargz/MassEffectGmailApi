71e68590216c6d2336a770af1033807278c1f15b
Christopher <mev412@gmail.com>
2016-11-26 15:36:05 -0600

    Renamed get_auth_url to create_credentials

--- create_credentials.py
+++ create_credentials.py
@@ -2,46 +2,56 @@ from __future__ import print_function
 
 import httplib2
 import os
-import json
 import sys
 
-import base64
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-from oauth2client.client import AccessTokenCredentials
-
 from apiclient.discovery import build
 
+#1 - get authorize url
+#2 - navigate to authorize url + grant permissions to the app
+#3 - redirect to redirect_url with authorize_code passed in query string ?code=
+#4 - exchange code for credentials, credentials.to_json() and from_json() can be used to save and retrieve
+#5 - use credentials to authenticate and use gmail api
 
+# cmdline args
+email_address = sys.argv[1]
 
-#result = get_authorization_url('mev412@gmail.com', 'nope', 'secret.json', scope);
-
-
-authorization_code = sys.argv[1]
-#authorization_code = '4/CMf5Bl7CZB1MPpLhpY2ZgomVnaZEWC0LWTS8RSPUGow'
-
-credential_file = 'credentials.json'
+# constants
 secret_file = 'secret.json'
-email_address = 'mev412@gmail.com'
-state='nope'
-scope = 'https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/gmail.modify'
+credential_file = 'credentials.json'
+scope = 'https://www.googleapis.com/auth/gmail.modify'
+#scope = 'https://www.googleapis.com/auth/gmail.readonly'
 
 flow = client.flow_from_clientsecrets(secret_file, scope)
+flow.params['user_id'] = email_address
 flow.params['access_type'] = 'offline'
 flow.params['approval_prompt'] = 'force'
-flow.params['user_id'] = email_address
-flow.params['state'] = state
-flow.redirect_uri = 'http://googleapi.sonyar.info'
+flow.params['state'] = 'nope'
 
-def save_creds():
+
+def get_authorization_url(email_address, secret_file, redirect_url):
+    return flow.step1_get_authorize_url(redirect_url)
+
+
+def save_creds(authorization_code):
     credentials = flow.step2_exchange(authorization_code)
     f = open(credential_file, 'w')
     f.write(credentials.to_json())
     f.close()
 
 
+
+redirect_url = 'http://googleapi.sonyar.info'
+
+url = get_authorization_url('mev412@gmail.com', scope, redirect_url);
+print(url)
+
+auth_code = raw_input("Navigate to above url in browser, grant permissions, after redirect paste the code from the query string: ")
 # save credentials
-save_creds()
+save_creds(auth_code)
+
+
 
